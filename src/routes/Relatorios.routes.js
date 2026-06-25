7f8d06d85b2fa4aca32d7ad53bd2569755db049c
Laboratório 203 <lab203@fiemguba.srv>
2026-06-25 19:23:46 -0300

    Alteração2

--- src/routes/Relatorios.routes.js
+++ src/routes/Relatorios.routes.js
@@ -6,11 +6,6 @@ const controller = require('../controllers/Relatorios.controller');
 router.post('/gerar', controller.gerar);
 router.post('/exportar', controller.exportar);
 router.get('/arquivo/:nome', controller.baixar);
-
-console.log(
-    JSON.stringify(registros[0], null, 2)
-);
-
 router.get('/ocupacao-hoje', controller.ocupacaoHoje);
 
 module.exports = router;
